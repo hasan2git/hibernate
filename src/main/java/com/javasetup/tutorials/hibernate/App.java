c375870dd3b379eee971d16c29f0c62124e719d8
hasan2git <hasanath.it@gmail.com>
2022-11-16 21:15:35 +0530

    Added new message

--- src/main/java/com/javasetup/tutorials/hibernate/App.java
+++ src/main/java/com/javasetup/tutorials/hibernate/App.java
@@ -30,6 +30,7 @@ public class App
        st.setDressColor("Blue");
        st.setAge(22);
        System.out.println("Inside....Hibenate app main");
+       System.out.println("Inside....new message");
        //Configuration config=new Configuration().configure().addAnnotatedClass(Student.class);
 //       Configuration configuration = new Configuration();
 //       configuration.addAnnotatedClass(Student.class);
